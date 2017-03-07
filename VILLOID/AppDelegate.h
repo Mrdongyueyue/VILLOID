6046e94093efaae94acaf30122667e0c482bab0d
董知樾 <dongzhiyue@dongzhiyuedeMacBook-Pro.local>
2017-03-07 19:49:47 +0800

    Initial Commit

---
+++ VILLOID/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VILLOID
+//
+//  Created by 董知樾 on 2017/3/7.
+//  Copyright © 2017年 董知樾. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VILLOID/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  VILLOID
+//
+//  Created by 董知樾 on 2017/3/7.
+//  Copyright © 2017年 董知樾. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
